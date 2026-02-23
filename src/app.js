190280ab78b89a2bc5af226411d48baa650b8b23
davittchelidze1 <davit.tchelidze.1@btu.edu.ge>
2026-02-23 22:37:31 +0400

    adding humidity

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname,'../public')
@@ -105,7 +106,6 @@ app.get('*',(req,res)=>{
     })
 })
 
-const port = process.env.PORT || 3000
 
 app.listen(port, ()=>{
     console.log('Server is up on port ' + port)
